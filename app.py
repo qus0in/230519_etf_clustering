4ed2b5a9a42e282a4cb52045585b46de0a64fda3
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2023-05-19 14:01:48 +0900

    mean

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ kwd = ["레버리지", "2X", "금리"]
 kwd_filter = [f"name.str.contains('{k}')" for k in kwd]
 fillteredEtfList = etfList.query(
     "category != 1 and not (" + " or ".join(kwd_filter) + ")"
-    + f" and trade_volume > {etfList.trade_volume.quantile(.5)}"
+    + f" and trade_volume > {etfList.trade_volume.mean()}"
     + f" and market_cap > {etfList.market_cap.mean()}"
 )
 st.dataframe(fillteredEtfList)
