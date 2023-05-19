a99c61a704138aece5ce2aa954f937a7fa161be0
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2023-05-19 13:52:15 +0900

    has

--- app.py
+++ app.py
@@ -10,6 +10,6 @@ etfList = get_etf_list(
 )
 kwd = ["레버리지"]
 fillteredEtfList = etfList.query(
-    "category != 1 and @kwd in name"
+    "category != 1 and (@kwd in name)"
 )
 st.dataframe(fillteredEtfList)
