a91b2e535210e8edc004f491bfa3d5a4b5d3a6c2
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2023-05-19 14:04:47 +0900

    중간값

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ kwd = ["레버리지", "2X", "금리", "단기채권", "단기통안채"]
 kwd_filter = [f"name.str.contains('{k}')" for k in kwd]
 fillteredEtfList = etfList.query(
     "category != 1 and not (" + " or ".join(kwd_filter) + ")"
-    + f" and trade_volume > {etfList.trade_volume.mean()}"
+    + f" and trade_volume > {etfList.market_cap.quantile(.5)}"
     + f" and market_cap > {etfList.market_cap.quantile(.5)}"
 )
 st.dataframe(fillteredEtfList)
