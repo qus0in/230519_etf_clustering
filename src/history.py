c606b7a3fbb3e94e1f932490d4f995852f51c6af
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2023-05-19 15:43:36 +0900

    200

--- src/history.py
+++ src/history.py
@@ -4,7 +4,7 @@ import streamlit as st
 def get_history(code, days):
     df = _get_data(code)
     if len(df) < days:
-        raise Exception("상장일 {days}일 미만")
+        raise Exception(f"상장일 {days}일 미만")
     return df
 
 @st.cache_data
