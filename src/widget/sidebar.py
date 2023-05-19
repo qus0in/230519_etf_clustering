cae127a049368ee9c6d4dae26fe64cd64d5205bd
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2023-05-19 18:56:05 +0900

    sidebar

--- src/widget/sidebar.py
+++ src/widget/sidebar.py
@@ -15,10 +15,10 @@ _CLEAR_BUTTON_OPTION = {
 
 def build():
     with st.sidebar:
-        quantile_slider("거래금액", "trade_volume", 0.7)
-        quantile_slider("시가총액", "market_cap", 0.7)
+        quantile_slider("거래금액", "trade_volume", 0.5)
+        quantile_slider("시가총액", "market_cap", 0.5)
         st.slider("상장일", key="history_days",
-                  min_value=100, max_value=500, step=50, value=200)
+                  min_value=100, max_value=300, step=50, value=200)
         ban_kwd_expander(FILTER_KWD)
         st.button(**_LOAD_BUTTON_OPTION)
         st.button(**_CLEAR_BUTTON_OPTION)
