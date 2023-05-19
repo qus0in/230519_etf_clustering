2b64718786fb88553cfcf77231fec03d02a6fd22
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2023-05-19 18:41:24 +0900

    2

--- src/data/etf_list.py
+++ src/data/etf_list.py
@@ -39,7 +39,7 @@ def get_etf_list(dto: EtfListRequestDTO=None):
 
 def filter_etf_list(etfList, volume = 0.5, cap = 0.5):
     kwd_filter = [f"item_name.str.contains('{k}')" for k in FILTER_KWD]
-    category = [2, 4, 5]
+    category = [2]
     query = "(category in @category) and not (" + " or ".join(kwd_filter) + ")"\
         + f" and trade_volume > {etfList.trade_volume.quantile(st.session_state.trade_volume_quantile)}"\
         + f" and market_cap > {etfList.market_cap.quantile(st.session_state.market_cap_quantile)}"
