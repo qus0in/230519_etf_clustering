d2e7e6836ecfdf584006dc2dcd7357bfab856151
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2023-05-19 15:16:40 +0900

    header

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
 from src.etf_list import get_etf_list, filter_etf_list, FILTER_KWD
 from src.history import get_history
 from src.enums import *
-from src.widget import quantile_slider, ban_kwd_expander
+from src.widget import header
 import streamlit as st
 
-col1, col2 = st.columns(2)
-with col1: quantile_slider("거래금액", "trade_volume", 0.7)
-with col2: quantile_slider("시가총액", "market_cap", 0.7)
+header()
 
 etfs = get_etf_list()
-ban_kwd_expander(FILTER_KWD)
 filtered_etfs = filter_etf_list(etfs)
 # st.dataframe(
 #     filtered_etfs,
