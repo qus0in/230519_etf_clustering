1e0082f9ad91c3ecd54cd504cc4bc9afd024a90b
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2023-05-19 14:29:35 +0900

    kwd

--- app.py
+++ app.py
@@ -6,7 +6,13 @@ import streamlit as st
 quantile_slider("거래금액", "trade_volume")
 quantile_slider("시가총액", "market_cap")
 
-kwd = ["레버리지", "2X", "금리", "단기채권", "단기통안채"]
+kwd = [
+    "레버리지", "2X", "배당", "TRF", "TDF",
+    "방어", "가치", "BBIG",
+    "금리", "단기채권", "단기통안채", "단기자금",
+    "머니마켓", "삼성", "ESG", "현대", "TSMC", "테슬라",
+    "TIGER TOP10", 
+    ]
 etfs = get_etf_list()
 st.dataframe(filter_etf_list(etfs, kwd),
              use_container_width=True)
