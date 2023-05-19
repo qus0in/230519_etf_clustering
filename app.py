a07aad6ba392d23855cfce4761a328bc47d5eb00
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2023-05-19 14:07:55 +0900

    filtering

--- app.py
+++ app.py
@@ -2,6 +2,13 @@ from src.etf_list import *
 from src.enums import *
 import streamlit as st
 
+def filtering(etfList, kwd, volume = 0.5, cap = 0.5):
+    kwd_filter = [f"name.str.contains('{k}')" for k in kwd]
+    query = "category != 1 and not (" + " or ".join(kwd_filter) + ")"\
+        + f" and trade_volume > {etfList.trade_volume.quantile(volume)}"\
+        + f" and market_cap > {etfList.market_cap.quantile(cap)}"
+    return etfList.query(query)
+
 etfList = get_etf_list(
     EtfListRequestDTO(
         EtfType.전체,
@@ -9,10 +16,4 @@ etfList = get_etf_list(
     )
 )
 kwd = ["레버리지", "2X", "금리", "단기채권", "단기통안채"]
-kwd_filter = [f"name.str.contains('{k}')" for k in kwd]
-fillteredEtfList = etfList.query(
-    "category != 1 and not (" + " or ".join(kwd_filter) + ")"
-    + f" and trade_volume > {etfList.market_cap.quantile(.5)}"
-    + f" and market_cap > {etfList.market_cap.quantile(.5)}"
-)
-st.dataframe(fillteredEtfList)
+st.dataframe(filtering(etfList, kwd, volume, cap))
