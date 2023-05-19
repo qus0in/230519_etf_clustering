5e650bedd444e0025b52bbff8db43b22b40e099a
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2023-05-19 13:50:25 +0900

    kwd

--- app.py
+++ app.py
@@ -3,7 +3,14 @@ from src.enums import *
 import streamlit as st
 
 etfList = get_etf_list(
-    EtfListRequestDTO(EtfType.전체,
-                  TargetColumn.시가총액)
-).query("category != 1")
-st.dataframe(etfList)
+    EtfListRequestDTO(
+        EtfType.전체,
+        TargetColumn.시가총액
+    )
+)
+kwd = ["레버리지"]
+fillteredEtfList = etfList.query(
+    "category != 1"/
+    "and @kwd in name"
+)
+st.dataframe(fillteredEtfList)
