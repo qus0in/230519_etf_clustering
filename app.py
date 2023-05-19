a27cbe00b824f7a47e0d85df5e8acccd758e0efb
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2023-05-19 15:55:19 +0900

    melting

--- app.py
+++ app.py
@@ -6,18 +6,22 @@ import streamlit as st
 sidebar()
 
 if st.session_state.run:
-    with st.spinner("ETF 리스트 정보 받아오기"):
+    with st.spinner("🫠 ETF 리스트를 불러오는 중입니다"):
         etfs = get_etf_list()
         filtered_etfs = filter_etf_list(etfs)
 
     histories = {}
     error = {}
-    for ticker in filtered_etfs.ticker:
+    tickers = filtered_etfs.ticker
+    progress_text = "🫠 거래 데이터를 불러오는 중입니다"
+    bar = st.progress(0, text=progress_text)
+    for idx in range(len(tickers)):
+        ticker = tickers[idx]
         try:
             histories[ticker] = get_history(ticker, st.session_state.history_days)
         except:
             error[ticker] = filtered_etfs[filtered_etfs.ticker == ticker].item_name
-
+        bar.progress((idx + 1) / len(tickers) * 100, text=progress_text)
     st.write(error)
 else:
     st.info("ready...")
