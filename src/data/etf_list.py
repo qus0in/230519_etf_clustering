4fef0e62a9b67e28d40d6c7336504f114feeb5c3
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2023-05-19 19:23:43 +0900

    ft

--- src/data/etf_list.py
+++ src/data/etf_list.py
@@ -37,13 +37,14 @@ def get_etf_list(dto: EtfListRequestDTO=None):
     }, inplace=True)
     return df
 
-def filter_etf_list(etfList, volume = 0.5, cap = 0.5):
+def filter_etf_list(etfList):
     kwd_filter = [f"item_name.str.contains('{k}')" for k in FILTER_KWD]
     category = [2]
     query1 = "(category in @category) and not (" + " or ".join(kwd_filter) + ")"
-    query2 = f"trade_volume > {etfList.trade_volume.quantile(st.session_state.trade_volume_quantile)}"\
-        + f" and market_cap > {etfList.market_cap.quantile(st.session_state.market_cap_quantile)}"
-    return etfList.query(query1).query(query2).reset_index(drop=True)
+    eftList2 = etfList.query(query1)
+    query2 = f"trade_volume > {eftList2.trade_volume.quantile(st.session_state.trade_volume_quantile)}"\
+        + f" and market_cap > {eftList2.market_cap.quantile(st.session_state.market_cap_quantile)}"
+    return .query(query2).reset_index(drop=True)
 
 @st.cache_data(show_spinner=False)
 def _get_data(params: dict):
