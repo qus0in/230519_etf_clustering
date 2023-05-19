f43f1ab69335941404b8fd35e4e005dafe0e7ebc
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2023-05-19 17:09:03 +0900

    data

--- src/widget/chart.py
+++ src/widget/chart.py
@@ -8,23 +8,24 @@ from src.data.history import *
 
 def dendrogram():
     data = st.session_state.history
-    df = pd.concat(data, axis=1)
-    df_er = get_earning_rate(df)
+    st.write(data)
+    # df = pd.concat([d.Close for d in data], axis=1)
+    # df_er = get_earning_rate(df)
 
-    # 상관계수 행렬 계산
-    corr_matrix = df_er.corr()
+    # # 상관계수 행렬 계산
+    # corr_matrix = df_er.corr()
 
-    # 거리 행렬로 변환
-    dist_matrix = 1 - corr_matrix.abs()
+    # # 거리 행렬로 변환
+    # dist_matrix = 1 - corr_matrix.abs()
 
-    # 덴드로그램 그리기
-    fig = ff.create_dendrogram(corr_matrix.values, labels=corr_matrix.columns)
+    # # 덴드로그램 그리기
+    # fig = ff.create_dendrogram(corr_matrix.values, labels=corr_matrix.columns)
 
-    fig.update_layout(
-        title='덴드로그램',  # 그래프 제목
-        xaxis={'title': '클러스터'},  # x축 제목
-        yaxis={'title': '거리'},  # y축 제목
-        height=600
-    )
+    # fig.update_layout(
+    #     title='덴드로그램',  # 그래프 제목
+    #     xaxis={'title': '클러스터'},  # x축 제목
+    #     yaxis={'title': '거리'},  # y축 제목
+    #     height=600
+    # )
     
-    st.plotly_chart(fig, use_container_width=True)
+    # st.plotly_chart(fig, use_container_width=True)
